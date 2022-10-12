3de5f1f15edaeece75e626d756fb9ad8ec7288ee
Gerardo Gonzalez <gerardo@dispatchland.mx>
2022-10-12 12:24:11 -0500

    Add - Templates folder for render the blocks in PHP

--- blocks/blocks.php
+++ blocks/blocks.php
@@ -4,7 +4,8 @@
  *
  * @package UnderstrapChild
  * @author Gerardo Gonzalez
- * @version 2022.09.26
+ * @version 2022.10.12
+ * @since 2022.09.26
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  * 
  */
@@ -24,10 +25,23 @@ $understrapchild_blocks = array(
  * through the block editor in the corresponding context.
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ * 
+ * To use the callback function please check the link below.
+ * To create the Template, put the file in the Tempaltes folder with the registered name of the block.
+ * 
+ * @see https://happyprime.co/2021/09/14/using-block-json-to-register-a-custom-gutenberg-block/
+ * 
  */
 function theme_register_block() {
 	foreach ( $understrapchild_blocks as $file ) {
-		register_block_type( __DIR__ . $understrapchild_build_dir.$file );
+		register_block_type( __DIR__ . $understrapchild_build_dir.$file, array('render_callback' => 'get_render_callback') );
 	}
 }
 add_action( 'init', 'theme_register_block' );
+
+function get_render_callback($attributes) {
+	ob_start();
+	$name = explode("/", $attributes["name"])[1];
+	get_template_part( 'blocks/templates/template', $name, $attributes);
+	return ob_get_clean();
+}
